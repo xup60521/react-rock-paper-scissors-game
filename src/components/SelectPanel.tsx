13ae149301e4c6e147a1777710086b5b88a67bd7
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-07-01 22:55:22 +0800

    fix startviewtransition when it is not supported

--- src/components/SelectPanel.tsx
+++ src/components/SelectPanel.tsx
@@ -8,13 +8,19 @@ export default function SelectPanel(props: {
     setPicked: React.Dispatch<React.SetStateAction<Picked>>;
 }) {
     function setPicked(e: Picked) {
-        const {setPicked: set} = props
-        const document2 = document as unknown as Document & {startViewTransition: (a: ()=>void)=>void}
-        document2.startViewTransition(()=>{
-            flushSync(()=>{
-                set(e)
-            })
-        })
+        const { setPicked: set } = props;
+        const document2 = document as unknown as Document & {
+            startViewTransition: (a: () => void) => void;
+        };
+        if ("startViewTransition" in document2) {
+            document2.startViewTransition(() => {
+                flushSync(() => {
+                    set(e);
+                });
+            });
+        } else {
+            set(e);
+        }
     }
     return (
         <div className="w-full flex flex-col gap-5">
@@ -22,14 +28,22 @@ export default function SelectPanel(props: {
                 <div className="absolute w-32 h-3 top-[5rem] bg-slate-900"></div>
                 <div className="absolute w-32 h-3 origin-top-left rotate-[60deg] -translate-x-12 top-[5rem] bg-slate-900"></div>
                 <div className="absolute w-32 h-3 origin-top-right -rotate-[60deg] translate-x-12 top-[5rem] bg-slate-900"></div>
-                <div id="paper-img" onMouseDown={()=>setPicked("paper")} className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-blue-800">
+                <div
+                    id="paper-img"
+                    onMouseDown={() => setPicked("paper")}
+                    className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-blue-800"
+                >
                     <div className="rounded-full border-[1.2rem] border-blue-500 w-full h-full">
                         <div className="border-t-[6px] border-gray-300 w-full h-full flex items-center justify-center bg-white rounded-full">
                             <IconPaper />
                         </div>
                     </div>
                 </div>
-                <div id="scissors-img" onMouseDown={()=>setPicked("scissors")} className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-yellow-800">
+                <div
+                    id="scissors-img"
+                    onMouseDown={() => setPicked("scissors")}
+                    className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-yellow-800"
+                >
                     <div className="rounded-full border-[1.2rem] border-yellow-400 w-full h-full">
                         <div className="border-t-[6px] border-gray-300 w-full h-full flex items-center justify-center bg-white rounded-full">
                             <IconScissors />
@@ -38,7 +52,11 @@ export default function SelectPanel(props: {
                 </div>
             </div>
             <div className="w-full flex justify-center relative">
-                <div id="rock-img" onMouseDown={()=>setPicked("rock")} className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-red-800">
+                <div
+                    id="rock-img"
+                    onMouseDown={() => setPicked("rock")}
+                    className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-red-800"
+                >
                     <div className="rounded-full border-[1.2rem] border-red-500 w-full h-full">
                         <div className="border-t-[6px] border-gray-300 w-full h-full flex items-center justify-center bg-white rounded-full">
                             <IconRock />
