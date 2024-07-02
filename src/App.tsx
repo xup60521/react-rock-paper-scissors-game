8d4cf66fb8a5a921abf6e4a1ed5806e40bab9bcf
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-07-02 17:49:35 +0800

    finish mobile design

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,30 @@ import IconRock from "@/assets/images/icon-rock.svg?react";
 import { useGame } from "./lib/hooks";
 
 export default function App() {
-    const { myPick, housePick,score } = useGame();
+    const { myPick, housePick, score } = useGame();
     return (
         <div
-            className="min-h-screen flex-col flex justify-center items-center gap-4"
+            className="min-h-screen flex-col flex justify-center items-center gap-4 overflow-x-hidden"
             style={{
                 background:
                     "radial-gradient(circle at top, hsl(214, 47%, 23%) 0%, hsl(237, 49%, 15%) 50%)",
             }}
         >
-            <div className="flex flex-col gap-12 lg:w-[45vw] flex-grow items-center py-8 relative">
-                <div className="rounded-2xl border-2 border-c_border_outline px-6 py-4 flex justify-between w-full">
+            <div className="flex flex-col gap-12 lg:w-[45vw] px-4 w-full flex-grow items-center py-8 relative">
+                <div className="rounded-2xl border-2 border-c_border_outline px-6 py-4 flex justify-between w-full items-center">
                     <div className="flex flex-col">
-                        <span className="font-barlow text-white font-bold text-[2.25rem] leading-8">
+                        <span className="font-barlow text-white font-bold text-2xl lg:text-[2.25rem] lg:leading-8 leading-5">
                             ROCK
                         </span>
-                        <span className="font-barlow text-white font-bold text-[2.25rem] leading-8">
+                        <span className="font-barlow text-white font-bold text-2xl lg:text-[2.25rem] lg:leading-8 leading-5">
                             PAPER
                         </span>
-                        <span className="font-barlow text-white font-bold text-[2.25rem] leading-8">
+                        <span className="font-barlow text-white font-bold text-2xl lg:text-[2.25rem] lg:leading-8 leading-5">
                             SCISSORS
                         </span>
                     </div>
-                    <div className="flex flex-col items-center justify-center bg-white rounded-lg lg:w-32">
-                        <span className="font-barlow text-c_score_text font-semibold ">
+                    <div className="flex flex-col items-center justify-center bg-white rounded-lg lg:w-32 w-24 py-2 h-full">
+                        <span className="font-barlow text-c_score_text font-semibold  lg:text-md text-sm">
                             SCORE
                         </span>
                         <span className="font-barlow text-[3rem] text-c_dark_text font-bold -my-3">
@@ -38,19 +38,18 @@ export default function App() {
                         </span>
                     </div>
                 </div>
-                {/* <button onMouseDown={()=>transition(()=>setPicked(""))}>reset</button> */}
                 {myPick === "" && <SelectPanel />}
                 {myPick !== "" && (
-                    <div className="grid grid-cols-2 w-full relative">
+                    <div className="grid grid-cols-2 w-full relative lg:scale-100">
                         <div
-                            className={`flex flex-col gap-12 items-center transition ${
-                                housePick !== "" && "-translate-x-[35%]"
+                            className={`flex lg:flex-col flex-col-reverse lg:gap-12 items-center transition ${
+                                housePick !== "" && "lg:-translate-x-[35%]"
                             }`}
                         >
-                            <span className="font-barlow font-semibold text-white text-xl tracking-widest">
+                            <span className="font-barlow font-semibold text-white lg:mt-0 -mt-6 lg:text-xl tracking-widest">
                                 YOU PICKED
                             </span>
-                            <div className="relative">
+                            <div className="relative lg:scale-100 scale-[0.55]">
                                 <WinGradient
                                     open={whoWins(myPick, housePick) === "p1"}
                                 />
@@ -59,14 +58,14 @@ export default function App() {
                         </div>
                         <Result state={whoWins(myPick, housePick)} />
                         <div
-                            className={`flex flex-col gap-12 items-center relative transition ${
-                                housePick !== "" && "translate-x-[35%]"
+                            className={`flex lg:flex-col flex-col-reverse lg:gap-12 items-center relative transition ${
+                                housePick !== "" && "lg:translate-x-[35%]"
                             }`}
                         >
-                            <span className="font-barlow font-semibold text-white text-xl tracking-widest">
+                            <span className="font-barlow font-semibold text-white lg:mt-0 -mt-6 lg:text-xl tracking-widest">
                                 THE HOUSE PICKED
                             </span>
-                            <div className="relative">
+                            <div className="relative lg:scale-100 scale-[0.55]">
                                 <WinGradient
                                     open={whoWins(myPick, housePick) === "p2"}
                                 />
@@ -79,13 +78,27 @@ export default function App() {
         </div>
     );
 }
-function Result({state}:{state: "p1" | "p2" | "draw" | ""}) {
-    const text = state === "p1" ? "YOU WIN" : (state === "p2" ? "YOU LOSE" : "DRAW")
-    const {newGame} = useGame()
-    return <div className={`absolute gap-2 left-[50%] flex flex-col items-center -translate-x-[50%] top-[50%] -translate-y-[50%] transition ${state === "" ? "scale-0" : "scale-100"}`}>
-        <span className="font-barlow text-white px-3 text-[2.5rem] font-bold">{text}</span>
-        <button onMouseDown={()=>newGame()} className="w-full py-2 transition hover:text-red-700 rounded-md bg-white font-barlow text-sm">PLAY AGAIN</button>
-    </div>
+function Result({ state }: { state: "p1" | "p2" | "draw" | "" }) {
+    const text =
+        state === "p1" ? "YOU WIN" : state === "p2" ? "YOU LOSE" : "DRAW";
+    const { newGame } = useGame();
+    return (
+        <div
+            className={`absolute gap-2 z-50 left-[50%] flex flex-col items-center -translate-x-[50%] lg:top-[50%] lg:-translate-y-[50%] top-[18rem] transition lg:w-fit w-[11.5rem] ${
+                state === "" ? "scale-0" : "scale-100"
+            }`}
+        >
+            <span className="font-barlow text-white px-3 text-[2.5rem] font-bold">
+                {text}
+            </span>
+            <button
+                onMouseDown={() => newGame()}
+                className="w-full lg:py-2 py-3 transition hover:text-red-700 rounded-md bg-white font-barlow text-sm"
+            >
+                PLAY AGAIN
+            </button>
+        </div>
+    );
 }
 function DisplayPick(props: {
     picked: Picked;
--- src/components/SelectPanel.tsx
+++ src/components/SelectPanel.tsx
@@ -7,7 +7,7 @@ export default function SelectPanel() {
     const { setPicked } = useGame();
 
     return (
-        <div className="w-full flex flex-col gap-5">
+        <div className="w-full flex flex-col gap-5 lg:scale-100 scale-90">
             <div className="flex gap-16 justify-center relative">
                 <div className="absolute w-32 h-3 top-[5rem] bg-slate-900"></div>
                 <div className="absolute w-32 h-3 origin-top-left rotate-[60deg] -translate-x-12 top-[5rem] bg-slate-900"></div>
@@ -15,7 +15,7 @@ export default function SelectPanel() {
                 <div
                     id="paper-img"
                     onMouseDown={() => setPicked("paper")}
-                    className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-blue-800"
+                    className="transition flex-shrink-0 hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-blue-800"
                 >
                     <div className="rounded-full border-[1.2rem] border-blue-500 w-full h-full">
                         <div className="border-t-[6px] border-gray-300 w-full h-full flex items-center justify-center bg-white rounded-full">
@@ -26,7 +26,7 @@ export default function SelectPanel() {
                 <div
                     id="scissors-img"
                     onMouseDown={() => setPicked("scissors")}
-                    className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-yellow-800"
+                    className="transition flex-shrink-0 hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-yellow-800"
                 >
                     <div className="rounded-full border-[1.2rem] border-yellow-400 w-full h-full">
                         <div className="border-t-[6px] border-gray-300 w-full h-full flex items-center justify-center bg-white rounded-full">
@@ -39,7 +39,7 @@ export default function SelectPanel() {
                 <div
                     id="rock-img"
                     onMouseDown={() => setPicked("rock")}
-                    className="transition hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-red-800"
+                    className="transition flex-shrink-0 hover:scale-110 cursor-pointer size-[10rem] z-20 rounded-full border-b-[8px] border-red-800"
                 >
                     <div className="rounded-full border-[1.2rem] border-red-500 w-full h-full">
                         <div className="border-t-[6px] border-gray-300 w-full h-full flex items-center justify-center bg-white rounded-full">
